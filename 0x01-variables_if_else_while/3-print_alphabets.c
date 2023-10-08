f95bd53a53067dd23ae57cc69a7c0756b5e2b54d
MowafY93 <mowafy793@gmail.com>
2023-10-08 04:56:53 +0000

    committing

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     **/
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A'
+		while (ch <= 'z')
+		{
+			putchr(ch);
+			ch++;
+		}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+		return (0);
+	}
