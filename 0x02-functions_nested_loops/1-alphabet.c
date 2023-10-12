f448946f064d6ebe1481ec09dc1bcee2ea4b5469
MowafY93 <mowafy793@gmail.com>
2023-10-12 01:18:48 +0000

    committing

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  *  * main - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ *     *     **/
+void print_alphabet(void)
+{
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
+}
