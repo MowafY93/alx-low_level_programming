4c73df1257fe4239b9174305048f55b15e03bad5
MowafY93 <mowafy793@gmail.com>
2023-10-12 02:28:30 +0000

    committing

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+i#include"main.h"
+/**
+ *  *  * print_alphabet_x10 - function that prints alphabets 10 times
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ *     *     **/
+void print_alphabet_x10(void)
+{
+int line, ch;
+
+for(line = 0; line <= 9; line++)
+{
+for(ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
+}
+}
